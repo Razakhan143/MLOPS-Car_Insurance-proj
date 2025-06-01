c05c6e911680826022f2b5512d268e32bdb056d1
Raza Khan <razakhanzada100@gmail.com>
2025-06-01 20:48:37 +0500

    cicd-test-2

--- demo.py
+++ demo.py
@@ -21,7 +21,6 @@
 #     logging.info(e)
 #     raise MyException(e, sys) from e
 
-
 # --------------------------------------------------------------------------------
 
 from src.pipline.training_pipeline import TrainPipeline
